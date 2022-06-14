68be3c0a2255797a9b576d29f09a64c7066659e9
Porcescu Dmitri <71259338+Urarak234@users.noreply.github.com>
2022-06-14 16:29:13 +0300

    Add files via upload

---
+++ Console Story/ConsoleStory/Enemies.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleStory
+{
+    internal class Enemies
+    {
+        public int hp;
+        public int atk;
+        public bool fight = false;
+        public void Dark_liq()
+        {
+            hp = 20;
+            atk = 2;
+            fight = true;
+        }
+        public void Strange_Darkness()
+        {
+            hp = 20;
+            atk = 6;
+            fight = true;
+        }
+
+        public void Nictorine_Darkness(int stage)
+        {
+            hp = 25;
+            atk = 9;
+            fight = true;
+            switch (stage)
+            {
+                case 0: hp = 25;
+                    break;
+                case 1: hp = 30;
+                    break;
+                case 2: hp = 35;
+                    break;
+                
+                default:
+                    break;
+            }
+        }
+
+        public void Dark_Foggy()
+        {
+            hp = 20;
+            atk = 10;
+            fight = true;
+        }
+
+        public void Cyclope_Soldier()
+        {
+            hp = 25;
+            atk = 11;
+            fight = true;
+        }
+        //----------------------------BOSSES!-----------------------
+
+        public void GroolGaalag()
+        {
+            hp = 30;
+            atk = 13;
+            fight = true;
+        }
+    }
+}
---
+++ Console Story/ConsoleStory/Items_Classes.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleStory
+{
+    internal class Items_Classes
+    {
+        public string name = "";
+        public int hp;
+        public int atk;
+        public int def;
+        public bool weapon_equiped = false;
+        public bool armour_equiped = false;
+        public int id;
+        public void Dwarf()
+        {
+            hp = 10;
+            atk = 4;
+            def = 6;
+        }
+        public void Human()
+        {
+            hp = 9;
+            atk = 5;
+            def = 5;
+        }
+        public void Elf()
+        {
+            hp = 8;
+            atk = 6;
+            def = 4;
+        }
+        //--------------------------------------------Equipement-----------------------------------
+        //                                   -----==== Weapons ====-----
+
+        public void Stick()
+        {
+            atk = 1;
+            id = 0;
+        }
+        public void Sword()
+        {
+            atk = 2;
+            id = 0;
+        }
+        public void Axe()
+        {
+            atk = 4;
+            id = 0;
+        }
+
+        public void Cursed_Dagger()
+        {
+            atk = 5;
+            hp -= 1;
+            id = 0;
+        }
+
+        //                                   -----==== Armour ====-----
+        public void Leather_Bracers()
+        {
+            def = 1;
+            id = 1;
+        }
+        public void Helmet()
+        {
+            def = 3;
+            id = 1;
+        }
+        public void Ornate_Helmet()
+        {
+            def = 4;
+            id = 1;
+        }
+        public void black_Helmet()
+        {
+            def = 5;
+            hp = 2;
+            atk = 1;
+            id = 1;
+        }
+
+        public void Reset()
+        {
+            atk = 0;
+            def = 0;
+            hp = 0;
+        }
+
+    }
+}
